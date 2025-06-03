55c76e9af51c3ed7041e8dbd257b909854b3d27b
kkopera <kornelia.a.kopera@gmail.com>
2025-06-03 17:26:00 +0200

    UI3

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -11,13 +11,12 @@ namespace BallerinaCappucina01
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
-    name: "Default",
-    url: "{controller}/{action}/{id}",
-    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-);
+       name: "Default",
+       url: "{controller}/{action}/{id}",
+       defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+   );
+
 
         }
     }
